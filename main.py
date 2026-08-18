b7fa7d9ad1462afd8d75fd92b3994ca5883ac939
Tropycal689 <fishymarica@gmail.com>
2026-08-18 03:36:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,4 +95,4 @@ async def setup_tickets(ctx):
 # --- INITIALIZATION ---
 keep_alive()
 
-bot.run(os.environ.get("MTUzODk2OTU2ODk3Mjc3MTM0OA.GYX8h0.DbJOvJnijZXInDsH58j9nHaxKkT4e16BfY-zwY"))
+bot.run(os.getenv("MTUzODk2OTU2ODk3Mjc3MTM0OA.GE1pFN.zz4X_Kcd5HpYzwVO8rzMAlVrn_a9lvTpjoZyfE"))
